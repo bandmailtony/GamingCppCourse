8664a219401c4504ed9ec6dfdba4190d50a2589d
Tony Pugliese <anthonyc.pugliese@gmail.com>
2025-08-03 22:57:16 -0400

    Test push, first commit, using terminal command

---
+++ L3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void pp(int i)
+{
+    std::cout << &i << " " << i << " " << sizeof(i) << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+    int a = 10;
+    int b = 25;
+
+    pp(a);
+    pp(b);    
+
+    return 0; 
+}
